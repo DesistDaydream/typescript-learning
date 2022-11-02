e1f770570e80cb0f5e13e63de7073adb73fefba2
DesistDaydream <373406000@qq.com>
2022-11-02 13:55:09 +0800

    原始数据类型

--- 1_data_type/primitive_data_type.ts
+++ 1_data_type/primitive_data_type.ts
@@ -1,9 +1,14 @@
-// TS 原始数据类型
+// 如果一个文件不带有顶级的 import 或 export 声明，那么它的内容被视为全局可见的（因此对模块也是可见的）。
+// 如果不写 export{}，则这个文件中的所有变量都是全局变量，
+// 比如在其他文件也声明了 isDone 变量，那么将会报错：
+// 无法重新声明块范围变量“isDone”。ts(2451)
+export {}
 
-// Boolean
+// TS 原始数据类型
+// Boolean(布尔)
 let isDone: boolean = false
 
-// Number
+// Number(数字)
 let decLiteral: number = 6
 let hexLiteral: number = 0xf00d
 let binaryLiteral: number = 0b1010
@@ -11,7 +16,7 @@ let octalLiteral: number = 0o744
 let notANumber: number = NaN
 let infinityNumber: number = Infinity
 
-// String
+// String(字符串)
 let myName: string = "Tom"
 let myAge: number = 25
 let sentence: string = `Hello, my name is ${myName}.
@@ -51,8 +56,9 @@ let list: any[] = [1, true, "free"]
 list[1] = 100
 
 // Void(空)
-function warnUser(): void {
-  alert("这是我的警告消息")
+// 若函数没有返回值，则函数的类型就是 void
+let voidFunc = function voidFuncDemo() {
+  console.log("把鼠标移动到 voidFunc 上，可以看到它的类型是 void")
 }
 
 let unusable: void = undefined

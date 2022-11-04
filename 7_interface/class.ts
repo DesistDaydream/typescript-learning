d498c9c21b98a32388774e5b48fae0d94bb5bc47
DesistDaydream <373406000@qq.com>
2022-11-04 10:31:18 +0800

    类的修饰符

--- 7_interface/class.ts
+++ 7_interface/class.ts
@@ -7,7 +7,6 @@ class Person {
   name: string
   // 定义属性时，还可以同时设置默认值
   age: number = 20
-
   // constructor 是关键字，用于构造函数
   constructor(name: string, age: number) {
     this.name = name
@@ -29,3 +28,38 @@ interface Person {
   age: number
   sayHello(): void
 }
+
+// 类的修饰符
+// public 修饰符，表示公有属性。
+// private 修饰符，表示私有属性。
+// protected 修饰符，表示受保护的属性。
+// 若不写修饰符，**默认为 public**。私有属性无法从外部调用，也无法被继承。
+class PersonTwo {
+  // public 修饰符，表示公有属性。
+  public name: string
+  // private 修饰符，表示私有属性。
+  private male: string
+  // protected 修饰符，表示受保护的属性。
+  protected sayHi() {
+    console.log("hi")
+  }
+  // readonly 修饰符，表示只读属性。
+  public readonly age: number = 20
+  // 若不写修饰符，**默认为 public**。私有属性无法从外部调用，也无法被继承。
+  // 比如下面 new PersonTwo() 后，如果编写 p.male 将会报错：属性“male”为私有属性，只能在类“Person”中访问。
+
+  // constructor 是关键字，用于构造函数
+  constructor(name: string, age: number) {
+    this.name = name
+    this.male = ""
+    this.age = age
+  }
+
+  public sayHello() {
+    console.log("hello")
+  }
+}
+
+let p2 = new PersonTwo("Tom", 18)
+// console.log(p2.male) // 报错：属性“male”为私有属性，只能在类“Person”中访问。
+// p2.age = 30 // 报错：无法分配到 "age" ，因为它是只读属性。

c448d367888b2099f5ab8ea59f6d1af4fd7941ad
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2022-11-26 17:02:48 +0800

    深拷贝与浅拷贝

--- 6_object/other.ts
+++
@@ -1,9 +0,0 @@
-// 深拷贝与浅拷贝
-let objOne = { name: "Tom", age: 18, info: { address: "北京" } }
-// 注意这里的 ...，这是 ES6 中的扩展运算符，用来拷贝对象。但是这里只是浅拷贝。
-let objTwo = { ...objOne }
-
-objOne.name = "Jerry"
-objOne.info.address = "上海"
-console.log(objOne)
-console.log(objTwo) // { name: 'Tom', age: 18, info: { address: '上海' } }

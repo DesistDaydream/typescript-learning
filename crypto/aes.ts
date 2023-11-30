1ac583002696269bb4b844ff54dad54db6f44410
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2023-11-30 21:47:11 +0800

    添加带有 import 语法的 ts 代码示例，同步修改 package.json 和 tsconfig.json
    Create .gitignore

---
+++ crypto/aes.ts
@@ -0,0 +1,28 @@
+// https://blog.51cto.com/u_12632800/5192084
+import CryptoJS from "crypto-js";
+
+//AES加密
+function encrypt(text: string): string {
+  return CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(key), {
+    // iv: "0102030405060708",      //偏移向量，如果加密模式是ECB的话，则不需要iv
+    mode: CryptoJS.mode.ECB, //加密模式
+    padding: CryptoJS.pad.Pkcs7, //填充
+  }).toString();
+}
+
+//AES解密
+function decrypt(text: string): string {
+  const result = CryptoJS.AES.decrypt(text, CryptoJS.enc.Utf8.parse(key), {
+    // iv: "0102030405060708",       //偏移向量，如果加密模式是ECB的话，则不需要iv
+    mode: CryptoJS.mode.ECB, //加密模式
+    padding: CryptoJS.pad.Pkcs7, //填充
+  });
+  return result.toString(CryptoJS.enc.Utf8);
+}
+
+const key: string = "1234567890123456"; //秘钥key
+const text: string = "DesistDaydream"; //要加密的字符串
+const text_encode: string = encrypt(text); //加密后的字符串
+
+console.log("加密的字符串为：", decrypt(text_encode)); //打印出解密后的字符串
+console.log("加密后的字符串为：", text_encode); //打印出加密后的字符串
--- generic/generic.ts
+++ generic/generic.ts
@@ -8,35 +8,35 @@ function GenericFunc<T>(param: T): T {
 }
 // 调用函数时，可以指定泛型的具体类型
 // 这个 string 可以理解为 GenericFunc() 函数中泛型的实参
-let result = GenericFunc<string>("hello")
+const result = GenericFunc<string>("hello")
 console.log(result)
 // 也可以不指定泛型的具体类型，让编译器自动推断
-let result2 = GenericFunc("hello")
+const result2 = GenericFunc("hello")
 console.log(result2)
 // 泛型的好处是可以在不指定具体类型的情况下，使用泛型的函数或类，可以支持多种类型的数据。
 // 比如下面的函数，可以接收任意类型的数组，然后返回数组中的第一个元素
 function firstElement<T>(arr: T[]): T {
   return arr[0]
 }
-let firstElementResult = firstElement([1, 2, 3])
+const firstElementResult = firstElement([1, 2, 3])
 console.log(firstElementResult)
 
 // 多个泛型参数
 function swap<T, U>(t: T, u: U): [U, T] {
   return [u, t]
 }
-let swapResult = swap<string, number>("hello", 123)
+const swapResult = swap<string, number>("hello", 123)
 console.log(swapResult)
 
 // 泛型中，类型的 默认值
 function createArray<T = string>(length: number, value: T): T[] {
-  let result: T[] = []
+  const result: T[] = []
   for (let i = 0; i < length; i++) {
     result[i] = value
   }
   return result
 }
-let createArrayResult = createArray(3, "x")
+const createArrayResult = createArray(3, "x")
 console.log(createArrayResult)
 
 // 泛型约束
@@ -48,7 +48,7 @@ interface PersionIF<N extends string | number, G> {
 }
 // 调用时，如果传入的参数不是指定的类型，就会报错
 // 比如：如果写成 PersionIF<boolean,string> 将会报错：类型“boolean”不满足约束“string | number”。
-let persion: PersionIF<string, string> = {
+const persion: PersionIF<string, string> = {
   name: "DesistDaydream",
   getName() {
     return this.name

50e68ca9ddb5abe62f9d03f7084f409b2a4e2352
LiChenhao <39847025+DesistDaydream@users.noreply.github.com>
2024-01-15 11:09:11 +0800

    update

--- crypto/aes.ts
+++ crypto/aes.ts
@@ -1,5 +1,5 @@
 // https://blog.51cto.com/u_12632800/5192084
-import CryptoJS from "crypto-js";
+import CryptoJS from "crypto-js"
 
 //AES加密
 function encrypt(text: string): string {
@@ -7,7 +7,7 @@ function encrypt(text: string): string {
     // iv: "0102030405060708",      //偏移向量，如果加密模式是ECB的话，则不需要iv
     mode: CryptoJS.mode.ECB, //加密模式
     padding: CryptoJS.pad.Pkcs7, //填充
-  }).toString();
+  }).toString()
 }
 
 //AES解密
@@ -16,13 +16,13 @@ function decrypt(text: string): string {
     // iv: "0102030405060708",       //偏移向量，如果加密模式是ECB的话，则不需要iv
     mode: CryptoJS.mode.ECB, //加密模式
     padding: CryptoJS.pad.Pkcs7, //填充
-  });
-  return result.toString(CryptoJS.enc.Utf8);
+  })
+  return result.toString(CryptoJS.enc.Utf8)
 }
 
-const key: string = "1234567890123456"; //秘钥key
-const text: string = "DesistDaydream"; //要加密的字符串
-const text_encode: string = encrypt(text); //加密后的字符串
+const key: string = "1234567890123456" //秘钥key
+const text: string = "DesistDaydream" //要加密的字符串
+const text_encode: string = encrypt(text) //加密后的字符串
 
-console.log("加密的字符串为：", decrypt(text_encode)); //打印出解密后的字符串
-console.log("加密后的字符串为：", text_encode); //打印出加密后的字符串
+console.log("加密的字符串为：", decrypt(text_encode)) //打印出解密后的字符串
+console.log("加密后的字符串为：", text_encode) //打印出加密后的字符串
